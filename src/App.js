b55a240661c5eaed5ffc7a0ab16e430b29166a8c
keepwe <1374854656@qq.com>
2018-09-08 19:59:50 +0800

    public part

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import Dhheader from "./components/Dhheader"
+import Logo from "./components/Logo"
+import Navfooter from "./components/Navfooter"
+import Footer from "./components/Footer"
+import Navheader from "./components/Navheader"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      	<div id="header">
+        	<Dhheader />
+        	<Logo />
+        	<Navheader />
+        </div>
+        
+        <h1>路由视图容器,高版写法router+route</h1>
+        
+        <Navfooter />
+        <Footer />
       </div>
     );
   }
